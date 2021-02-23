c156bb114325a78188ac7fe5197e35eef7572d6e
Leon Groot Bruinderink <lgrootbr@gmail.com>
2021-02-23 14:37:55 +0100

    WIP solver with jobs and several machines

--- scheduler/src/linearsolver.rs
+++ scheduler/src/linearsolver.rs
@@ -2,6 +2,7 @@
 mod tests {
     use coin_cbc::{raw::Status, Model, Sense};
     use itertools::Itertools;
+    use array_tool::vec::Intersect;
 
     /*
     Job shop problem.
@@ -23,10 +24,116 @@ mod tests {
         sum_{j,k} b_{i,j,k} == 1 for all i
      */
 
+    /*
+    Not covered in this example: a job that can be executed by one of the machines:
+    Say job j is executable on machine 1 or 2, then make a copy of job j and add constraints like:
+
+    bin{1,2} binary variable
+
+    c_{1j} <= bin{1,2}*M
+    c_{2j} <= (1-bin{1,2})*M
+
+    c_{1j} + dur_j * bin{1,2} <= makespan
+    c_{2j} + dur_j * (1-bin{1,2}) <= makespan
+
+    c_{1j} + dur_j*bin{1,2} <= c_{ik} + b_{i,j,k} * M for all i,j,k
+    c_{2j} + dur_j*(1-bin{1,2}) <= c_{ik} + b_{i,j,k} * M for all i,j,k
+     */
+
+    //machines, duration, id
+    #[derive(Clone, Debug)]
+    pub struct JobWithID(u32, i32, u32);
+
     #[derive(Clone, Debug)]
     pub struct Job(u32, i32);
 
+    #[derive(Clone, Debug)]
+    pub struct Jobj(Vec<usize>, Vec<i32>);
+
     pub const BIG_NUM: f64 = 10000 as f64;
+
+    #[test]
+    fn test_jobswithmultiplemachines() {
+        /*let jobs_data = vec![JobWithID(0,3,0),JobWithID(1,3,0), JobWithID(2,3,0) JobWithID(1, 2, 1), JobWithID(2, 2, 2),
+        JobWithID(0, 2, 3), JobWithID(2, 1, 4), JobWithID(1, 4, 5),
+        JobWithID(1, 4, 6), JobWithID(2, 3, 7)];
+         */
+
+        let jobs_data = vec![Jobj(vec![0], vec![3]), Jobj(vec![1], vec![2]), Jobj(vec![2], vec![2]),
+        Jobj(vec![0], vec![2]), Jobj(vec![2], vec![1]), Jobj(vec![1], vec![4]), Jobj(vec![1], vec![4]), Jobj(vec![2], vec![3])];
+
+        const num_machines: usize = 3;
+        const num_jobs: usize = 8;
+
+        let mut m = Model::default();
+
+        let mut columns = vec![];
+
+        //makespan = col[0] >= 0
+        columns.push(m.add_integer());
+        let mut row = m.add_row();
+        m.set_row_lower(row, 0.0);
+        m.set_weight(row, columns[0], 1.0);
+
+        for job in jobs_data.iter(){
+            let index = m.num_cols() as usize;
+            for _ in 0..num_machines{
+                columns.push(m.add_binary());
+            }
+            for _ in 0..(num_jobs*num_machines){
+                columns.push(m.add_binary());
+            }
+            let index_sv = m.num_cols() as usize;
+            columns.push(m.add_integer()); //s_v
+            let index_ev = m.num_cols() as usize;
+            columns.push(m.add_integer()); //e_v
+            let index_pv = m.num_cols() as usize;
+            columns.push(m.add_integer()); //p_v
+
+            let row_onemachine = m.add_row();
+            let row_onetime = m.add_row();
+            for machine in 0..num_machines{
+                if job.0.iter().any(|&i| i==machine){
+                    m.set_weight(row_onemachine, columns[index.clone()+machine], 1.0);
+                    m.set_weight(row_onetime, columns[index.clone()+machine], job.1[0] as f64);
+                }
+            }
+            m.set_row_lower(row_onemachine, 1.0);
+            m.set_row_upper(row_onemachine, 1.0);
+
+            m.set_weight(row_onetime, columns[index_pv.clone()], -1.0);
+            m.set_row_lower(row_onetime, 0.0);
+            m.set_row_upper(row_onetime, 0.0);
+
+            row = m.add_row();
+            m.set_row_lower(row, 0.0);
+            m.set_weight(row, columns[index_sv.clone()], 1.0);
+
+            row = m.add_row();
+            m.set_row_lower(row, 0.0);
+            m.set_row_upper(row, 0.0);
+            m.set_weight(row, columns[index_ev.clone()], -1.0);
+            m.set_weight(row, columns[index_sv.clone()], 1.0);
+            m.set_weight(row, columns[index_pv.clone()], 1.0);
+
+            row = m.add_row();
+            m.set_row_upper(row,0.0);
+            m.set_weight(row, columns[0], -1.0);
+            m.set_weight(row, columns[index_ev.clone()],1.0);
+        }
+
+        assert_eq!(m.num_cols() as usize, 1 + num_jobs*(num_machines + num_jobs*num_machines + 3));
+
+        for (job_i, job_j) in jobs_data.iter().tuple_combinations() {
+            let index_i = jobs_data.iter().position(|&r| r == job_i).unwrap();
+            let index_j = jobs_data.iter().position(|&r| r == job_j).unwrap();
+            let machines = job_i.0.intesect(job_j.0);
+            for m in machines.iter(){
+                
+            }
+        }
+    }
+
 
     pub fn flatten_jobs(input_data: Vec<Vec<Job>>) -> Vec<(u32, i32, usize)> {
         let mut result = vec![];
@@ -59,8 +166,6 @@ mod tests {
         let mut row = m.add_row();
         m.set_row_lower(row, 0.0);
         m.set_weight(row, columns[0], 1.0);
-
-        //c_{ij} + d_j <= c_{i,j+1}
 
         for joblist in jobs_data.iter() {
             let n = columns.len();
@@ -114,6 +219,9 @@ mod tests {
                 m.set_weight(row, columns[n], BIG_NUM);
             }
         }
+
+        assert_eq!(m.num_cols(),16);
+        assert_eq!(m.num_rows(),36);
 
         m.set_obj_coeff(columns[0], 1.);
         // Set objective sense.
