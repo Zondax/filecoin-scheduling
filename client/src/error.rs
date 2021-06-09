529a3dc0d1f8d772154506966431b7dbc101c218
neithanmo <neithanmo@gmail.com>
2021-06-08 23:14:43 -0600

    remove this options which are not necessary
    Show the error message for mutex/scheduler errors
    
     cosmetic changes to the lock file name

--- client/src/error.rs
+++ client/src/error.rs
@@ -3,13 +3,13 @@ use std::io::Error as IoError;
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-    #[error("GlobalMutex error")]
+    #[error("GlobalMutex error: `{0}`")]
     GlobalMutex(#[from] IoError),
     #[error("Rpc client error: `{0}`")]
     RpcError(String),
     #[error("Timeout")]
     Timeout,
-    #[error("Scheduler error")]
+    #[error("Scheduler error: `{0}`")]
     Scheduler(#[from] SchedulerError),
     #[error("Can not parse the provided address")]
     InvalidAddress,
